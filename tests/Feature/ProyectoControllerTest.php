cde80f866d09489e787cfcc1a2c4e74425582e80
J.Alejandro Martínez Markman <106510785+Alejandro83658@users.noreply.github.com>
2024-11-25 18:21:57 +0100

    Cambio de nombre y clase al archivo test

--- tests/Feature/ProyectoControllerTest.php
+++ tests/Feature/ProyectoControllerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ControllersExerciseTest extends TestCase
+class ProyectoControllerTest extends TestCase
 {
     public function test_controladores(): void
     {
